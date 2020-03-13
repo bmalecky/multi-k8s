72e7349be6e7a20a74827ec8b78aee7e7a9097c2
bmalecky <bill.malecky@gmail.com>
2020-03-13 11:43:33 -0400

    travis yml changes3

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
 
 class App extends Component {
   render() {
