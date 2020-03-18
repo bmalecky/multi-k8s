7f96cda87f69ea82f803871ab0ef70dfdc21cb7b
bmalecky <bill.malecky@gmail.com>
2020-03-18 17:17:00 -0400

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t bmaleckysandbox/multi-client:latest -t bmaleckysandbox/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t bmaleckysandbox/multi-server:latest -t bmaleckysandbox/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t bmlaeckysandbox/multi-worker:latest -t bmaleckysandbox/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push bmaleckysandbox/multi-client:latest
+docker push bmaleckysandbox/multi-server:latest
+docker push bmaleckysandbox/multi-worker:latest
+
+docker push bmaleckysandbox/multi-client:$SHA
+docker push bmaleckysandbox/multi-server:$SHA
+docker push bmaleckysandbox/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/client-deployment server=bmaleckysandbox/multi-server:$SHA
+kubectl set image deployments/server-deployment server=bmaleckysandbox/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=bmaleckysandbox/multi-worker:$SHA
